c3f0f38ef6876fdc8f5d39b0d4f25cb4959a5b9d
sashaarlinskaya <113176286+sashaarlinskaya@users.noreply.github.com>
2024-11-14 11:32:35 +0300

    Add files via upload

---
+++ L3.2.js
@@ -0,0 +1,17 @@
+// Напишите функцию, которая умножает два числа
+function calculateMult(a, b) {
+    const mult = a*b;
+    console.log("Произведение чисел:" + mult)
+}
+
+calculateMult(5, 3);
+
+
+
+
+
+
+
+
+
+
---
+++ L3.3.js
@@ -0,0 +1,7 @@
+// Напишите функцию, которая возвращает длину строки
+function strlength(str) {
+    return str.length;
+}
+
+const result =  strlength("Hello world!");
+console.log("Длина строки:" + result);
